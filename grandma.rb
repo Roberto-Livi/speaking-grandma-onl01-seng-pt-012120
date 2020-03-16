18faf23981d1c6d2d6f4d2ccd5e7f3a2a62fcd36
Roberto Livi <robertomlivi@outlook.com>
2020-03-16 02:58:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,7 @@ def speak_to_grandma(string)
     "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.uppercase
     "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
     
